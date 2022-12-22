5f2843a9f0951f05e02abaa56884d64b03c07ede
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2022-12-22 14:13:11 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,15 +1,15 @@
 function solution(my_string) {
     var answer = '';
-    let arr = my_string.split('')
+    // let arr = my_string.split('')
     
-    for (let i of arr) {
+    for (let i of my_string) {
         if (isUpperCase(i)) {
             answer += i.toLowerCase()
         } else {
             answer += i.toUpperCase()
         }
     }
-    answer
+    
     return answer;
 }
 
