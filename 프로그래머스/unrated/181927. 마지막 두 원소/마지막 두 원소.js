345b237e183331c5b290b87eb681e4ea7ec6bfa3
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-23 14:51:27 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -10,3 +10,8 @@ function solution(num_list) {
         return num_list
     } 
 }
+
+// function solution(num_list) {
+//     const [a, b] = [...num_list].reverse();
+//     return [...num_list, a > b ? (a-b):a*2];
+// }
