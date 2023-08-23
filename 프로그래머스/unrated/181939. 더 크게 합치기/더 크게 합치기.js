9a42a906be51b76ce9d098b3d8d1327129098f03
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-23 17:30:11 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -3,3 +3,8 @@ function solution(a, b) {
     const ba = Number(String(b) + String(a))
     return ab === ba ? ab : Math.max(ab,ba) 
 }
+
+// 나 바본가?? 
+// function solution(a, b) {
+//     return Math.max(Number(`${a}${b}`), Number(`${b}${a}`))
+// }
