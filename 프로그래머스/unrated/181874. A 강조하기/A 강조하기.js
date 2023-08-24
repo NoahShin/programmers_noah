0c91dfe9380e23b56822ac15f2cc0ad3df140cb8
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-24 10:33:33 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.js
@@ -1,3 +1,7 @@
 function solution(myString) {
     return [...myString].map((x)=> x === 'a' ? x.toUpperCase() : x !== 'A' ? x.toLowerCase() : x).join('')
 }
+
+
+// 나 바본가..
+// const solution=s=>s.toLowerCase().replaceAll('a','A');
