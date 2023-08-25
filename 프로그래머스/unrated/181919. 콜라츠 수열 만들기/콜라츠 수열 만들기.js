e43a841fcab50ba8966c6c33468200bfd4c813b1
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-25 09:33:22 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  let arr = [];
+  
+  while (n > 0) {
+      arr.push(n)
+      
+      if (n === 1) {
+        break;
+      }
+      
+      if (n%2 === 0) {
+          n = n / 2
+      } else {
+          n = 3 * n + 1
+      }
+  }
+    return arr;
+}
+
