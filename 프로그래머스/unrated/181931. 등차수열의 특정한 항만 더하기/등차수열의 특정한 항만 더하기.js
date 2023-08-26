bb82cd2eaf27582ee5e30ad7674543cd7387f0c7
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-26 16:18:58 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -8,3 +8,10 @@ function solution(a, d, included) {
     return ans
     
 }
+
+// 그래.. reduce 를 끝까지 썼었어야했어..
+// function solution(a, d, included) {
+//     return included.reduce((acc, flag, i) => {
+//         return flag ? acc + a + d * i : acc
+//     }, 0)
+// }
