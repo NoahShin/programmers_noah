624b26d2a644a83756416a4ff23d19687f3bc1c8
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-26 15:06:50 +0900

    [unrated] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -6,3 +6,9 @@ function solution(str1, str2) {
     }
     return a;
 }
+
+// 난 바보였습니다...str1 이랑 str2 랑 길이가 같은 걸 생각하면 이렇게 map 돌려도 됬는데..
+
+// function solution(str1, str2) {
+//     return [...str1].map((x, idx)=> x+str2[idx]).join("");
+// }
