8ae1f5f5e4400628f44d23fd852580450101545c
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-30 12:13:41 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -0,0 +1,15 @@
+function solution(num_list, n) {
+    // 빈 배열 생성
+  const result = [];
+    
+  while(num_list.length > 0) {
+    let tempArray;
+    // splice() 메서드를 사용하여 특정 길이만큼 배열을 분리함
+    tempArray = num_list.splice(0, n);
+    // 빈 배열에 특정 길이만큼 분리된 배열을 추가
+    result.push(tempArray);
+  }
+    
+  return result;
+    
+}
