126b4b3823551210182c3dc0158d69b0a88ca40c
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-08-30 14:41:36 +0900

    [unrated] Title: 배열 만들기 5, Time: 1.35 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,3 +1,7 @@
 function solution(intStrs, k, s, l) {
     return intStrs.map((x)=> x.slice(s,l+s)).filter((x)=> Number(x) > k).map((x)=> Number(x))
 }
+
+// function solution(intStrs, k, s, l) {
+//     return intStrs.map((v) => +v.slice(s, s + l)).filter((v) => v > k);
+// }
