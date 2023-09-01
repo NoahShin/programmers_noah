7bdd1ff176075ccedbbe59fc10bcef6c4d334707
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-09-01 16:57:15 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.11 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -3,3 +3,5 @@ function solution(date1, date2) {
     return new Date(date1.join('.')) < new Date(date2.join('.')) ? 1: 0
     
 }
+// 이게 된다고?
+// const solution = (date1, date2) => new Date(date1) < new Date(date2) ? 1 : 0
