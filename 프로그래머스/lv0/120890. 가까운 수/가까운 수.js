e3ae97fac01f2db09d9e5ecf2a1f8cfe8bfaacb4
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-09-04 17:04:42 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -17,3 +17,18 @@ function solution(array, n) {
   return result;
 
 }
+
+// function solution(array, n) {
+//     array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b);
+
+//     return array[0];
+// }
+
+// function solution(array2, n) {
+//     var array = [...array2].sort()
+//     var cal = array.map(v => Math.abs(v-n)) //요소에서 n뺀것을 절대값으로 표시
+//     var cal2 = Math.min(...cal) //제일 가까운수의 값
+//     var cal3 = cal.indexOf(cal2) //제일 가까운수의 인덱스
+
+//     return array[cal3];
+// }
