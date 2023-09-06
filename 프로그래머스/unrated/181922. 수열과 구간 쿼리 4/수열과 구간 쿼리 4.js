90517f93144cb935b3ccfe47727a4eeb7eb9d18f
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-09-06 14:56:43 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 19.32 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,12 +1,14 @@
-function solution(arr, queries) {
-    return queries.reduce((acc, cur) => {
-        const copyArr = [...acc]
-        const [to, from, val] = cur
-        return copyArr.map((el, elIdx) => {
-            if(elIdx >= to && elIdx <= from) {
-                if(elIdx%val === 0) return el+1
-            }
-            return el
-        })
-    }, arr)
-}
+// function solution(arr, queries) {
+//     return queries.reduce((acc, cur) => {
+//         const copyArr = [...acc]
+//         const [to, from, val] = cur
+//         return copyArr.map((el, elIdx) => {
+//             if(elIdx >= to && elIdx <= from) {
+//                 if(elIdx%val === 0) return el+1
+//             }
+//             return el
+//         })
+//     }, arr)
+// }
+
+const solution=(a,q)=>q.length?solution(a.map((v,i)=>i>=q[0][0]&&i<=q[0][1]&&!(i%q[0][2])?v+1:v),q.slice(1)):a
