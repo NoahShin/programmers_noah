c82ab9a0a406c08533fa83aad6458f77f7214d79
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-09-06 14:21:53 +0900

    [unrated] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
+++ 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.js
@@ -1,20 +1,24 @@
 function solution(arr) {
-    const arrLength = arr.length;
-
-    // 주어진 배열의 길이가 이미 2의 정수 거듭제곱이라면 그대로 반환
-    if ((arrLength & (arrLength - 1)) === 0) {
-        return arr;
-    }
-
-    // 2의 정수 거듭제곱으로 만들기 위해 필요한 0의 개수 계산
-    const nextPowerOf2 = Math.pow(2, Math.ceil(Math.log2(arrLength)));
-    const numOfZerosToAdd = nextPowerOf2 - arrLength;
-
-    // 필요한 개수만큼 0을 배열 뒤에 추가하여 반환
-    const result = [...arr]; // 기존 배열을 변경하지 않기 위해 복사
-    for (let i = 0; i < numOfZerosToAdd; i++) {
-        result.push(0);
-    }
-
-    return result;
+    return [...arr, ...Array(2 ** Math.ceil(Math.log2(arr.length)) - arr.length).fill(0)];
 }
+
+// function solution(arr) {
+//     const arrLength = arr.length;
+
+//     // 주어진 배열의 길이가 이미 2의 정수 거듭제곱이라면 그대로 반환
+//     if ((arrLength & (arrLength - 1)) === 0) {
+//         return arr;
+//     }
+
+//     // 2의 정수 거듭제곱으로 만들기 위해 필요한 0의 개수 계산
+//     const nextPowerOf2 = Math.pow(2, Math.ceil(Math.log2(arrLength)));
+//     const numOfZerosToAdd = nextPowerOf2 - arrLength;
+
+//     // 필요한 개수만큼 0을 배열 뒤에 추가하여 반환
+//     const result = [...arr]; // 기존 배열을 변경하지 않기 위해 복사
+//     for (let i = 0; i < numOfZerosToAdd; i++) {
+//         result.push(0);
+//     }
+
+//     return result;
+// }
