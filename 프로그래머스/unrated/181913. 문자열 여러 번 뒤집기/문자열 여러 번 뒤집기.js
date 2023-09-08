45c5b1d96c65942a13f760411491d3dd6f6fd09a
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-09-08 13:47:58 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 68.33 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -9,3 +9,14 @@ function solution(my_string, queries) {
     })
   return arr.join('')
 }
+
+// 그래,,, splice 를 썼어야했어
+
+// function solution(my_string, queries) {
+//     let str = my_string.split('');
+//   queries.forEach(([start, end]) => {
+//     const changeStr = str.slice(start, end + 1);
+//     str.splice(start, changeStr.length, ...changeStr.reverse());
+//   });
+//   return str.join('');
+// }
