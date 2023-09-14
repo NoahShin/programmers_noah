f92ca1b7a6ba775dea977d3344426bbcc260a89f
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-09-14 13:49:47 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -11,3 +11,15 @@ function solution(lines) {
 
     return answer;
 }
+
+// for(; a < b; a++) line[a+100]++; 이부분을 생각을 못해서...
+
+// function solution(lines) {
+//     let line = new Array(200).fill(0);
+
+//     lines.forEach(([a, b]) => {
+//         for(; a < b; a++) line[a+100]++;
+//     });
+
+//     return line.reduce((a, c) =>  c > 1 ? a + 1 : a, 0)
+// }
