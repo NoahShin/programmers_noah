dedfcc6b93d522b01f2fd28df79572d9d20b9988
Jae Hoon Shin, Noah <69205911+NoahShin@users.noreply.github.com>
2023-09-15 11:33:35 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,5 +1,16 @@
-function solution(x) {
-    const aa = String(x).split('').reduce((acc,cur)=> acc += Number(cur),0)
-    return (x % aa == 0) ? true : false
+// function solution(x) {
+//     const aa = String(x).split('').reduce((acc,cur)=> acc += Number(cur),0)
+//     return (x % aa == 0) ? true : false
  
+// }
+
+function solution(x) {
+    let num = x;
+    let sum = 0;
+    do {
+        sum += x%10;
+        x = Math.floor(x/10);
+    } while (x>0);
+
+    return !(num%sum);
 }
